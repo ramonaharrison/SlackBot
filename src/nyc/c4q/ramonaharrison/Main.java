7636592bdbb1e54f153322ac1f7c07239ee8f225
Akasha Archere <akasha.archer@gmaile.com>
2016-09-13 15:23:08 -0400

    Completed TODOs in Attachment class

--- src/nyc/c4q/ramonaharrison/Main.java
+++ src/nyc/c4q/ramonaharrison/Main.java
@@ -15,10 +15,10 @@ public class Main {
         myBot.listMessages(Slack.BOTS_CHANNEL_ID);
 
         // Post "Hello, world!" to the #bots channel
-        //myBot.sendMessage("Hello, world!");
+        myBot.sendMessageToBotsChannel("Hello, world!");
 
         // Post a pineapple photo to the #bots channel
-        //myBot.sendMessage("http://weknowyourdreams.com/images/pineapple/pineapple-07.jpg");
+        myBot.sendMessageToBotsChannel("http://weknowyourdreams.com/images/pineapple/pineapple-07.jpg");
 
     }
 }
