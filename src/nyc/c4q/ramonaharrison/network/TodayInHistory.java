7801d944085f211dbc36c42f1c3752f7b03c5cdb
Rafael Soto <rooksoto@gmail.com>
2016-09-14 15:06:15 -0400

    Completed todos in Event.java

---
+++ src/nyc/c4q/ramonaharrison/network/TodayInHistory.java
@@ -0,0 +1,7 @@
+package nyc.c4q.ramonaharrison.network;
+
+/**
+ * Created by Mac on 9/14/16.
+ */
+public class TodayInHistory {
+}
---
+++ src/nyc/c4q/ramonaharrison/network/response/TodayInHistoryResponse.java
@@ -0,0 +1,7 @@
+package nyc.c4q.ramonaharrison.network.response;
+
+/**
+ * Created by Mac on 9/14/16.
+ */
+public class TodayInHistoryResponse {
+}
