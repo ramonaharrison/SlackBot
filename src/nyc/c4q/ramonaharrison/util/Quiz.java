efcf1ce03ff910d6f28d1ed8ca2085f079ac325a
Shannon Alexander-Navarro <shannonalexander@hotmail.com>
2016-09-17 21:20:57 -0400

    Shannon's 2nd Commit

--- src/nyc/c4q/ramonaharrison/util/Quiz.java
+++ src/nyc/c4q/ramonaharrison/util/Quiz.java
@@ -20,26 +20,28 @@ public class Quiz {
             List<Message> messages = listMessagesResponse.getMessages();
             String firstResponse = messages.get(0).getText();
             String secondResponse;
+            boolean isAnswerOk = true;
 
             if (firstResponse.equalsIgnoreCase("y") || firstResponse.equalsIgnoreCase("yes")) {
                 Slack.sendMessage("Aight fam, it's gonna go like this- I give you a word, you tell me what it means. You feel me? "
                         + "Pick a, b, or c, aight? "
                         + "\n1st question: The word is Situationship. As in \"Sounds like you in a situationship\"."
                         + "\na)A situation   b)Playing at being in a relationship without actually being in one  c)A committed relationship");
-              ListMessagesResponse listMessagesResponse1 = Slack.listMessages(Slack.BOTS_CHANNEL_ID);
-                messages = listMessagesResponse.getMessages();
-                secondResponse = messages.get(0).getText();
-                if (secondResponse.equalsIgnoreCase("b")) {
-                    Slack.sendMessage("I see you " + "this.getName()" + ". You obviously out in these streets, I'ma make sure I put some respek on your name.");
-                    Slack.sendMessage(Images.makeImages(4));
-                    playing = false;
-                } else if (secondResponse.equalsIgnoreCase("a") || secondResponse.equalsIgnoreCase("c")) {
-                    Slack.sendMessage("Good for you, obviously you ain't neva been in one-the answer was b.");
-                    Slack.sendMessage(Images.makeImages(1));
-                } else {
-                    Slack.sendMessage("Yo, a, b or c only homie.");
-                    Slack.sendMessage(Images.makeImages(0));
-                    playing = false;
+                while (isAnswerOk) {
+                    ListMessagesResponse listMessagesResponse1 = Slack.listMessages(Slack.BOTS_CHANNEL_ID);
+                    messages = listMessagesResponse1.getMessages();
+                    secondResponse = messages.get(0).getText();
+                    if (secondResponse.equalsIgnoreCase("b")) {
+                        Slack.sendMessage("I see you " + "this.getName()" + ". You obviously out in these streets, I'ma make sure I put some respek on your name.");
+                        Slack.sendMessage(Images.makeImages(4));
+                        playing = false;
+                        isAnswerOk = false;
+                    } else if (secondResponse.equalsIgnoreCase("a") || secondResponse.equalsIgnoreCase("c")) {
+                        Slack.sendMessage("Good for you, obviously you ain't neva been in one-the answer was b.");
+                        Slack.sendMessage(Images.makeImages(1));
+                        playing = false;
+                        isAnswerOk = false;
+                    }
                 }
 
             } else if (firstResponse.equalsIgnoreCase("n") || firstResponse.equalsIgnoreCase("no")) {
@@ -52,7 +54,7 @@ public class Quiz {
                 messages = listMessagesResponse.getMessages();
                 secondResponse = messages.get(0).getText();
                 if (secondResponse.equalsIgnoreCase("b")) {
-                    Slack.sendMessage("I see you " + "this.getName()" + ". You obviously out in these streets, I'ma make sure I put some respek on your name.");
+                    Slack.sendMessage("I see you. You obviously out in these streets, I'ma make sure I put some respek on your name.");
                     Slack.sendMessage(Images.makeImages(4));
                     playing = false;
                 } else if (secondResponse.equalsIgnoreCase("a") || secondResponse.equalsIgnoreCase("c")) {
