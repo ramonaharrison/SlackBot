7bbad1f0e4274acfa4dcdfb571d3f0647ff3ed7a
Shantal Taveras <shantaltaveras@ac.c4q.nyc>
2017-10-16 17:19:19 -0400

    update user.java to parse a json and get user fields

--- src/nyc/c4q/ramonaharrison/model/User.java
+++ src/nyc/c4q/ramonaharrison/model/User.java
@@ -14,23 +14,97 @@ import org.json.simple.JSONObject;
 public class User {
 
     // TODO: implement private fields for each of the following user JSON keys:
-    // "id"
-    // "name"
-    // "deleted"
-    // "color"
-    // "profile"
-    // "is_admin"
-    // "is_owner"
-    // "is_primary_owner"
-    // "is_restricted"
-    // "is_ultra_restricted"
+    private String id ;
+    private String name;
+    private boolean deleted = true;
+    private String color ;
+    private HashMap<String,String> profile = new HashMap<>();
+    private boolean is_admin;
+    private boolean is_owner;
+    private boolean is_restricted;
+    private boolean is_ultra_restricted;
+    private boolean has_2fa;
+
+
     // "has_2fa"
     // "two_factor_type"
     // "has_files"
 
     public User(JSONObject json) {
         // TODO: parse a user from the incoming json
+        if (json.get("id") != null){
+            this.id = (String) json.get("id");
+        }
+        if (json.get("name") != null){
+            this.name = (String) json.get("name");
+        }
+        if (json.get("deleted") != null){
+            this.deleted = (Boolean) json.get("deleted");
+        }
+        if (json.get("color") != null){
+            this.color = (String) json.get("color");
+        }
+        if (json.get("profile") != null){
+            this.profile = (HashMap<String, String>) json.get("profile");
+        }
+        if (json.get("is_admin") != null){
+            this.is_admin = (String) json.get("is_admin");
+        }
+        if (json.get("is_owner") != null){
+            this.is_owner = (String) json.get("is_owner");
+        }
+        if (json.get("is_restricted") != null){
+            this.is_restricted = (String) json.get("is_restricted");
+        }
+        if (json.get("is_ultra_restricted") != null){
+            this.is_ultra_restricted = (String) json.get("is_ultra_restricted");
+        }
+        if (json.get("has_2fa") != null){
+            this.has_2fa = (String) json.get("has_2fa");
+        }
+
+
+
     }
 
-    // TODO add getters to access private fields
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public HashMap<String, String> getProfile() {
+        return profile;
+    }
+
+    public boolean isIs_admin() {
+        return is_admin;
+    }
+
+    public boolean isIs_owner() {
+        return is_owner;
+    }
+
+    public boolean isIs_restricted() {
+        return is_restricted;
+    }
+
+    public boolean isIs_ultra_restricted() {
+        return is_ultra_restricted;
+    }
+
+    public boolean isHas_2fa() {
+        return has_2fa;
+    }
+
 }
