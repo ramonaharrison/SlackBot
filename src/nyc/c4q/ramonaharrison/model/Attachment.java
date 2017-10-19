73762a26dbe1ffcc9f6700ca3af60a565ccc5164
Amirah Oxendine <amirahoxendine@ac.c4q.nyc>
2017-10-19 15:14:41 -0400

    aj's Attachment.java TODO's completed

--- src/nyc/c4q/ramonaharrison/model/Attachment.java
+++ src/nyc/c4q/ramonaharrison/model/Attachment.java
@@ -31,10 +31,23 @@ public class Attachment {
     // "footer"
     // "footer_icon"
     // "ts"
-    String fallback;
-    String color;
-    String pretext;
-    String author_name;
+    private String fallback;
+    private String color;
+    private String pretext;
+    private String author_name;
+    private String author_link;
+    private String author_icon;
+    private String title;
+    private String title_link;
+    private String text;
+    private ArrayList<Field> fields = new ArrayList<>();
+    private String image_url;
+    private String thumb_url;
+    private String footer;
+    private String footer_icon;
+    private String ts;
+
+
 
     public Attachment(JSONObject json) {
         // TODO: parse an attachment from the incoming json
@@ -42,17 +55,55 @@ public class Attachment {
             this.fallback = (String) json.get("fallback");
         }
         if (json.get("color") != null) {
-            this.color = (String) json.get("fallback");
+            this.color = (String) json.get("color");
         }
         if (json.get("pretext") != null) {
-            this.pretext = (String) json.get("fallback");
+            this.pretext = (String) json.get("pretext");
         }
         if (json.get("author_name") != null) {
-            this.author_name = (String) json.get("fallback");
+            this.author_name = (String) json.get("author_name");
         }
-
-        //private List<Field> fields = new ArrayList<>();
+        if (json.get("author_link") != null) {
+            this.author_link = (String) json.get("author_link");
+        }
+        if (json.get("author_icon") != null) {
+            this.author_icon = (String) json.get("author_icon");
+        }
+        if (json.get("title_link") != null) {
+            this.title_link = (String) json.get("title_link");
+        }
+        if (json.get("title") != null) {
+            this.title = (String) json.get("title");
+        }
+        if (json.get("text") != null) {
+            this.text = (String) json.get("text");
+        }
+        if (json.get("fields") != null) {
+            this.fields = (ArrayList<Field>) json.get("fields");
+        }//TODO research how to get fields
+        if (json.get("image_url") != null) {
+            this.image_url = (String) json.get("image_url");
+        }
+        if (json.get("thumb_url") != null) {
+            this.thumb_url = (String) json.get("thumb_url");
+        }
+        if (json.get("footer") != null) {
+            this.footer = (String) json.get("footer");
+        }
+        if (json.get("footer_icon") != null) {
+            this.footer_icon = (String) json.get("footer_icon");
+        }
+        if (json.get("ts") != null) {
+            this.ts = (String) json.get("ts");
+        }
     }
+
+    private class Field{
+        String title;
+        String value;
+    }
+
+
 
     // TODO add getters to access private fields
 
